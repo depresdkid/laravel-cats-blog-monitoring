b7529425828de1dd94ce65c54a4de4208ca613a1
LightSof <egorikse@mail.ru>
2026-05-01 01:33:55 +0300

    Сделал модель уведомлений

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -3,8 +3,22 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Notification extends Model
 {
-    //
+    protected $fillable = [
+        'blog_id',
+        'report',
+    ];
+
+    protected $casts = [
+        'blog_id' => 'integer',
+        'report' => 'array',
+    ];
+
+    public function blog(): BelongsTo
+    {
+        return $this->belongsTo(Blog::class);
+    }
 }
